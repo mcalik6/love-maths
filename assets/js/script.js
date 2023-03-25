48cc8ef38e8e2817482dba7ebb6c028025eda648
mcalik6 <mcalik6@gmail.com>
2023-03-25 14:53:40 +0000

    round up

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,7 @@ function runGame(gameType) {
 
     document.getElementById("answer-box").value = "";
     document.getElementById("answer-box").focus();
+    
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -45,7 +46,7 @@ function runGame(gameType) {
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);}
         else if (gameType === "division" ) {
-            displaySubtractQuestion(num1, num2);
+            displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -148,8 +149,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 function displayDivisionQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
 
 }
