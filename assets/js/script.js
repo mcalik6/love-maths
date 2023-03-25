de95668e84408bddfc15a441a71c417fb4910d2c
mcalik6 <mcalik6@gmail.com>
2023-03-25 14:27:49 +0000

    not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,9 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
-        displaySubtractQuestion(num1, num2);
+        displaySubtractQuestion(num1, num2);}
+        else if (gameType === "division" ) {
+            displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -87,9 +89,14 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else if (operator === "-") {
+    } 
+    else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    }
+     else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    } 
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -137,5 +144,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
